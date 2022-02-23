8e1c723edd8387e27bbede4cf86a40a630bb8e86
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2022-02-23 14:48:14 -0600

    ADD: Backend/Agrega primeras rutas de la aplicacion

--- backend/src/configs/index.js
+++ backend/src/configs/index.js
@@ -4,13 +4,11 @@ const configs = (() => {
 
     const port = process.env.PORT || 5000;
     const node_env = process.env.USERSPA_ENV || 'develop';
-    const token = process.env.USERSPA_TOKEN;
     const cadenaConexion = process.env.USERSPA_CONECTION;
 
     return {
         port,
         node_env,
-        token,
         cadenaConexion,
     }
 })();
--- backend/src/routers/index.js
+++ backend/src/routers/index.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
-const {} = require("../services");
+const {
+    loginUser,
+    getGroups,
+    getGroup,
+    createGroup,
+    updateGroup,
+} = require("../services");
 
 router.route('/api/v1').get(async (req, res) => {
     res.status(200).json({
@@ -8,4 +14,33 @@ router.route('/api/v1').get(async (req, res) => {
     });
 });
 
-module.exports = router;
+router.route('/api/v1/users/login').post(async (req, res) => {
+    const body = req.body
+    const { status, response } = loginUser(body);
+    res.status(status).json(response);
+});
+
+router.route('/api/v1/grupos').get(async (req, res) => {
+    const { status, response } = getGroups();
+    res.status(status).json(response);
+});
+
+router.route('/api/v1/grupos/:id_grupo').get(async (req, res) => {
+    const { id_grupo } = req.params
+    const { status, response } = getGroup();
+    res.status(status).json(response);
+});
+
+router.route('/api/v1/grupos').post(async (req, res) => {
+    const body = req.body
+    const { status, response } = createGroup(body);
+    res.status(status).json(response);
+});
+
+router.route('/api/v1/grupos/:id_grupo').put(async (req, res) => {
+    const { id_grupo } = req.params
+    const { status, response } = updateGroup(id_grupo);
+    res.status(status).json(response);
+});
+
+module.exports = router
